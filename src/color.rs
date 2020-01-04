380c88dbf7cdd53ab744461ca8b9529a30002857
Sean Geoghegan <seangeo@gmail.com>
2020-01-04 21:58:09 +0800

    Implement color multiplication

--- src/color.rs
+++ src/color.rs
@@ -29,6 +29,30 @@ impl std::ops::Sub for Color {
     }
 }
 
+impl std::ops::Mul for Color {
+    type Output = Color;
+
+    fn mul(self, c: Color) -> Color {
+        Color {
+            r: self.r * c.r,
+            g: self.g * c.g,
+            b: self.b * c.b
+        }
+    }
+}
+
+impl std::ops::Mul<f64> for Color {
+    type Output = Color;
+
+    fn mul(self, s: f64) -> Color {
+        Color {
+            r: self.r * s,
+            g: self.g * s,
+            b: self.b * s
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,5 +81,18 @@ mod tests {
         let c1 = color(0.95, 0.6, 0.75);
         let c2 = color(0.7, 0.1, 0.25);
         assert_eq!(color(0.25, 0.5, 0.5), c1 - c2)
+    }
+
+    #[test]
+    fn multiplying_color_by_a_scalar() {
+        let c = color(0.2, 0.3, 0.4);
+        assert_eq!(color(0.4, 0.6, 0.8), c * 2.0)
+    }
+
+    #[test]
+    fn multiplying_color_by_a_color() {
+        let c1 = color(1.0, 0.2, 0.5);
+        let c2 = color(0.9, 1.0, 0.2);
+        assert_eq!(color(0.9, 0.2, 0.1), c1 * c2)
     }
 }
