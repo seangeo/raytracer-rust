67eadb6493012cc6de61d5e3c8360a89f74c1868
Sean Geoghegan <seangeo@gmail.com>
2020-02-21 21:34:37 +1030

    Support matrix multiplication.

--- src/matrix.rs
+++ src/matrix.rs
@@ -20,9 +20,40 @@ impl Matrix {
         }
     }
 
+    pub fn from_elementsi(elements: &[i64]) -> Matrix {
+        let elements: Vec<f64> = elements.iter().map(|&e| e as f64).collect();
+
+        Matrix::from_elements(&elements)
+    }
+
     pub fn get(&self, i: usize, j: usize) -> f64 {
         self.elements[self.size * i + j]
     }
+
+    pub fn row(&self, i: usize) -> Vec<f64> {
+        let mut result = vec![];
+
+        for j in 0..self.size {
+            result.push(self.get(i, j));
+        }
+
+        result
+    }
+
+    pub fn col(&self, j: usize) -> Vec<f64> {
+        let mut result = vec![];
+
+        for i in 0..self.size {
+            result.push(self.get(i, j));
+        }
+
+        result
+    }
+}
+
+fn dot(a: &[f64], b: &[f64]) -> f64 {
+    a.iter().zip(b).map(|(&a, &b)| a * b).sum()
+
 }
 
 impl std::cmp::PartialEq<Matrix> for Matrix {
@@ -38,6 +69,23 @@ impl std::cmp::PartialEq<Matrix> for Matrix {
                 all(|(&x, &y)| (x - y).abs() < EPSILON)
         }
     }
+}
+
+impl std::ops::Mul for Matrix {
+    type Output = Matrix;
+
+    fn mul(self, m: Matrix) -> Matrix {
+        let mut result: Vec<f64> = Vec::new();
+
+        for i in 0..self.size {
+            for j in 0..self.size {
+                result.push(dot(&self.row(i), &m.col(j)));
+            }
+        }
+
+        Matrix::from_elements(&result)
+    }
+
 }
 
 #[cfg(test)]
@@ -103,4 +151,22 @@ mod tests {
         let m2 = Matrix::from_elements(&vec![1.0, 2.0, 3.0, 4.0001]);
         assert_eq!(m1, m2)
     }
+
+    #[test]
+    fn can_multiply_two_matrixes() {
+        let m1v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2];
+        let m2v = vec![-2, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6,  5, 1, 2, 7, 8];
+        let resultv = vec![20, 22, 50, 48, 44, 54, 114, 108, 40, 58, 110, 102, 16, 26, 46, 42];
+        let m1 = Matrix::from_elementsi(&m1v);
+        let m2 = Matrix::from_elementsi(&m2v);
+        let result = Matrix::from_elementsi(&resultv);
+
+        assert_eq!(result, m1 * m2);
+    }
+
+    // #[test]
+    // #[should_panic]
+    // fn multiplying_different_sized_matrices_fails() {
+
+    // }
 }
