33285c9e5de03b022884e2638fc1ad5449724e95
Sean Geoghegan <seangeo@gmail.com>
2020-03-15 01:05:03 +1030

    Move pattern type into separate enum

--- src/pattern/mod.rs
+++ src/pattern/mod.rs
@@ -1,20 +1,12 @@
 use crate::{Color, Point};
 
-#[derive(Debug, PartialEq,Copy, Clone)]
-pub enum Pattern {
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum PatternType {
     Solid(Color),
     Stripe(Color, Color)
 }
 
-impl Pattern {
-    pub fn solid(c: Color) -> Pattern {
-        Self::Solid(c)
-    }
-
-    pub fn stripe(c1: Color, c2: Color) -> Pattern {
-        Self::Stripe(c1, c2)
-    }
-
+impl PatternType {
     pub fn color_at(&self, p: Point) -> Color {
         match self {
             Self::Solid(c) => *c,
@@ -28,6 +20,29 @@ impl Pattern {
         } else {
             c2
         }
+    }
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct Pattern {
+    pattern_type: PatternType
+}
+
+impl Pattern {
+    pub fn solid(c: Color) -> Pattern {
+        Pattern {
+            pattern_type: PatternType::Solid(c)
+        }
+    }
+
+    pub fn stripe(c1: Color, c2: Color) -> Pattern {
+        Pattern {
+            pattern_type: PatternType::Stripe(c1, c2)
+        }
+    }
+
+    pub fn color_at(&self, p: Point) -> Color {
+        self.pattern_type.color_at(p)
     }
 }
 
